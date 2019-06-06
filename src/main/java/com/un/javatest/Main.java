005f3085720acb9a78765d84cbbdd259275357f3
unlim <xiangchh@puwell.com>
2019-06-06 16:24:59 +0800

    1.save

--- src/main/java/com/un/javatest/Main.java
+++ src/main/java/com/un/javatest/Main.java
@@ -1,8 +1,38 @@
 package com.un.javatest;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+		Calendar calendar = Calendar.getInstance();
+		printCalendar(calendar);
+	}
+
+	public static final void printCalendar(Calendar calendar) {
+
+		DateFormat df = DateFormat.getDateTimeInstance();
+
+		System.out.println(df.format(calendar.getTime()));
+		System.out.println(calendar.toString());
+
+		int year = calendar.get(Calendar.YEAR);
+		int mouth = calendar.get(Calendar.MONTH);
+		//		int day = calendar.get(Calendar.DATE);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+		System.out.println("year:" + year + ";");
+		System.out.println("mouth:" + mouth + ";");
+		System.out.println("day:" + day + ";");
+
+		int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
+		System.out.println("dayOfYear:" + dayOfYear + ";");
+
+		//星期相关
+		calendar.setFirstDayOfWeek(Calendar.MONDAY);
+		int firstDayOfWeek = calendar.getFirstDayOfWeek();
+		System.out.println("firstDayOfWeek:" + firstDayOfWeek + ";");
+
 	}
 }
